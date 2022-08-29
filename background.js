c3951d72bf77042dc714213d4a00b47a6e8c4b75
Zbomb2000 <hxl9500@gmail.com>
2022-08-29 11:49:34 -0400

    backup before i break something

--- background.js
+++ background.js
@@ -1,25 +1,50 @@
+var toggle = 0;
+
 function reddenPage() {
   var delayVar = 0;
   var numVar = 0;
   var bg = document.body.style;
-  const colorlist = ["red", "orange", "yellow", "green", "blue", "purple"]
+  const colorlist = ["red", "orange", "yellow", "green", "blue", "purple"];
 
   var myfunc = setInterval(function() {
   	numVar = Math.floor(Math.random() * 6);
-  	bg.backgroundColor = colorlist[numVar]
+  	bg.backgroundColor = colorlist[numVar];
   	// No delay on open
   	if (delayVar == 0) {
   		delayVar = 1000;
   	}
+    if (toggle == 1) {
+      clearInterval();
+      console.log("Cleared thing");
+    }
 
-  }, delayVar)
+  }, delayVar);
+}
+
+function whittenPage() {
+  clearInterval();
+  document.body.style.backgroundColor = "white";
 }
 
 chrome.action.onClicked.addListener((tab) => {
   if(!tab.url.includes("chrome://")) {
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      function: reddenPage
-    });
+    if (toggle == 0) {
+      chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: reddenPage
+      });
+      toggle = 1;
+      console.log("toggle = 1");
+      console.log(toggle)
+    } else if (toggle == 1) {
+      console.log("whittenPage");
+      chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: whittenPage
+      });
+      toggle = 0;
+      console.log("toggle = 0");
+      console.log(toggle);
+    }
   }
 });
